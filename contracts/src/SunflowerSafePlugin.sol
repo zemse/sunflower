0744bb43f9a67c3257deac4e373dcda3cb7d91c8
Soham Zemse <22412996+zemse@users.noreply.github.com>
2023-07-22 13:35:35 +0200

    sunflower draft

---
+++ contracts/src/SunflowerSafePlugin.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {ISafe} from "@safe-global/safe-core-protocol/contracts/interfaces/Accounts.sol";
+import {ISafeProtocolManager} from "@safe-global/safe-core-protocol/contracts/interfaces/Manager.sol";
+import {ISafeProtocolPlugin} from "@safe-global/safe-core-protocol/contracts/interfaces/Integrations.sol";
+import {SafeProtocolAction, SafeTransaction, SafeRootAccess} from "@safe-global/safe-core-protocol/contracts/DataTypes.sol";
+
+import {BasePluginWithEventMetadata, PluginMetadata} from "@5afe/safe-core-protocol-demo/contracts/Base.sol";
+
+/// @title The Sunflower plugin for Gnosis Safe
+/// @notice This plugin is to be used for safes on L2 to use owners on L1 using zk proofs.
+contract SunflowerSafePlugin is
+    ISafeProtocolPlugin,
+    BasePluginWithEventMetadata
+{
+    // Cache is used to prevent using zk proofs to prove the list of owners for every action.
+    // After a long time, zk proof will be used to read owners from L1, however to reduce costs
+    // some expiry like 6 hours can be used so that the Safe users can perform back to back
+    // actions without using zk proofs every time.
+    struct Cache {
+        uint128 ttl;
+        uint128 blockTimestamp;
+        address[] owners;
+    }
+    mapping(ISafe => Cache) public cache;
+
+    constructor()
+        BasePluginWithEventMetadata(
+            PluginMetadata({
+                name: "Sunflower Plugin",
+                version: "0.0.1",
+                requiresRootAccess: true,
+                iconUrl: "",
+                appUrl: "https://5afe.github.io/safe-core-protocol-demo/#/relay/${plugin}"
+            })
+        )
+    {}
+
+    // inputs transaction & claimed owners & prooof & owner signatures
+    function executeTransactionThroughManager(
+        ISafeProtocolManager manager,
+        ISafe safe,
+        SafeProtocolAction calldata action,
+        uint8 operation,
+        bytes calldata zkProof,
+        bytes[] calldata l1OwnerSignatures
+    ) external {
+        address[] memory owners;
+
+        if (zkProof.length != 0) {
+            // TODO
+            // check if proof corresponds to correct owners and recent block
+            // save to cache
+        } else {
+            require(
+                _l2CurrentTimestamp() <
+                    cache[safe].blockTimestamp + cache[safe].ttl
+            );
+            owners = cache[safe].owners;
+        }
+
+        // TODO check signatures
+
+        // execute transaction
+        if (address(manager) != address(0)) {
+            // go through the manager as a mediator plugin.
+            // requires this plugin to be whitelisted in the manager plugin.
+            uint256 nonce = uint256(
+                keccak256(abi.encode(this, manager, safe, action))
+            );
+            if (operation == 0) {
+                SafeProtocolAction[] memory actions = new SafeProtocolAction[](
+                    1
+                );
+                actions[0] = action;
+                manager.executeTransaction(
+                    safe,
+                    SafeTransaction(actions, nonce, bytes32(0))
+                );
+            } else if (operation == 1) {
+                manager.executeRootAccess(
+                    safe,
+                    SafeRootAccess(action, nonce, bytes32(0))
+                );
+            } else {
+                revert("invalid opr");
+            }
+        } else {
+            // directly approach the gnosis safe.
+            // requires this plugin to be whitelisted in the gnosis safe.
+            safe.execTransactionFromModule(
+                action.to,
+                action.value,
+                action.data,
+                operation
+            );
+        }
+    }
+
+    /// @notice Allows overriding this function to use different L2s requirements.
+    function _l2CurrentTimestamp() public view virtual returns (uint) {
+        return block.timestamp;
+    }
+}
